8427ac1ffcf2c959bc1d6cef3790514dab27f801
Andrey Kumykov <aakumykov@yandex.ru>
2024-04-27 10:15:06 +0300

    Модуль demo вместо app для разрешения конфликта пространства имён.

---
+++ demo/src/main/java/com/github/aakumykov/storage_access_helper_demo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.aakumykov.storage_access_helper_demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainerView, StartingFragment.create(), null)
+            .commit()
+    }
+}
---
+++ demo/src/main/java/com/github/aakumykov/storage_access_helper_demo/openAppProperties.kt
@@ -0,0 +1,12 @@
+package com.github.aakumykov.storage_access_helper_demo
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+
+fun Activity.openAppProperties() {
+    val uri = Uri.parse("package:$packageName")
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, uri)
+    if (intent.resolveActivity(packageManager) != null) { startActivity(intent) }
+}
